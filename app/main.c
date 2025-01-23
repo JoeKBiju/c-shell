277566e98f49353cecc18b796badb24bd2ba0994
JoeKBiju <joebiju8@gmail.com>
2025-01-23 20:12:08 +0000

    added echo command

--- app/main.c
+++ app/main.c
@@ -2,6 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+int parseCommand(char* command, int len) {
+  char word[100];
+  int i = 0;
+  while (*(command+i) != ' ' && *(command+i) != '\0') {
+    word[i] = *(command + i);
+    i++;
+  }
+ 
+  word[i] = '\0';
+  if (!strcmp(word, "exit")) {
+    if (*(command + i+1) == '0') { return 1; }
+  } else if (!strcmp(word, "echo")) {
+    printf("%s\n", command+i+1);
+  } else {
+    printf("%s: command not found\n", command);
+  }
+  return 0;
+}
+
 int main() {
   // READ-EVAL-PRINT-LOOP
   while (1) {
@@ -18,10 +37,9 @@ int main() {
     //When inputing command the newline char is put into the buffer when pressing "ENTER".
     // '/0' is the NULL character whcich represents the end of a string.
     input[strlen(input)-1] = '\0';
-    if (!strcmp(input, "exit 0")) {
-      return 0;
-    }
-    printf("%s: command not found\n", input);
+    
+    int output = parseCommand(input, strlen(input));
+    if (output) { return 0; }
   }
   return 0;
 }
