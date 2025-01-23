d463cebf8d9f746e8ab5ad86e8c14ead28ccdee1
JoeKBiju <joebiju8@gmail.com>
2025-01-23 18:31:28 +0000

    handles invalid commands now

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Flush after every printf
@@ -9,5 +10,12 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  
+  //Handle Input
+  //When inputing command the newline char is put into the buffer when pressing "ENTER".
+  // '/0' is the NULL character whcich represents the end of a string.
+  input[strlen(input)-1] = '\0';
+  printf("%s: command not found\n", input);
+
   return 0;
 }
